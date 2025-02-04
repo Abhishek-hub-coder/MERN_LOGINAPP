865045b9d39b3bd8feb7208255d21c8c484bdbf9
Abhishek-hub-coder <sharmaabhishek33721@gmail.com>
2025-02-04 23:39:49 +0530

    gitignore updated

---
+++ server/config.js
@@ -0,0 +1,11 @@
+// config.js
+export default {
+    JWT_SECRET: "lwHzlhOIzaOFZtWlZECf04R9//jzZQ7c1wfyy47brH0=",
+    EMAIL: "fernando.yundt93@ethereal.email",
+    PASSWORD: "sZ9ZKpU14tsbU8kSGf",
+    ATLAS_URI: "mongodb+srv://admin:admin123@cluster0.en8eola.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+};
+
+
+
